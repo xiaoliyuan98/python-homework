faf48351ee3607f06712e7d2d59e87576c6fe960
xiaoliyuan98 <86616504+xiaoliyuan98@users.noreply.github.com>
2021-07-07 14:25:43 +0800

    Update 0004.py

--- 0004.py
+++ 0004.py
@@ -3,16 +3,16 @@ import re
 fin = open('D:/Pyhton学习/11.txt', 'r')
 strs = fin.read()
 
-reObj = re.compile('\b?(\w+)\b?')
-words = reObj.findall(strs)
+reObj = re.compile('\b?(\w+)\b?') #编译为一个pattern对象
+words = reObj.findall(strs) #取出对象
 
-wordDict = dict()
+wordDict = dict() #字典
 
-for word in words:
-    if word.lower() in wordDict:
-        wordDict[word.lower()] += 1
+for word in words:  #当属于对象时
+    if word.lower() in wordDict: #如果是小写
+        wordDict[word.lower()] += 1 #单词数+1
     else:
-        wordDict[word.lower()] = 1
+        wordDict[word.lower()] = 1 
 
 for key, value in wordDict.items():
     print('%s: %s' % (key, value))
