6256d39ec70af4c4e7513d99f6d93056ac66bf59
xiaoliyuan98 <86616504+xiaoliyuan98@users.noreply.github.com>
2021-07-07 11:24:42 +0800

    Create 0005.py

---
+++ 0005.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import os
+
+path = 'D:/Pyhton学习/img'
+resultPath = 'D:/Pyhton学习/result'
+if not os.path.isdir(resultPath):
+    os.mkdir(resultPath)
+for picName in os.listdir(path):
+    picPath = os.path.join(path, picName)
+    print(picPath)
+    with Image.open(picPath) as im:
+        w, h = im.size
+        n = w / 1366 if (w / 1366) >= (h / 640) else h / 640
+        im.thumbnail((w / n, h / n))
+        im.save(resultPath+'/finish_' + picName.split('.')[0] + '.jpg', 'jpeg')
